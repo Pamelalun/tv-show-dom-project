8876729a724245d1cd25ee7680a9c4b33cbe5f98
Pamelalun <105378916+Pamelalun@users.noreply.github.com>
2023-02-01 11:02:09 +0000

    update script.js

--- script.js
+++ script.js
@@ -3,10 +3,40 @@ function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
-
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  for (const episode of episodeList) {
+  const nameOfEpisode = episode.name;
+  const nameDiv = document.createElement("div");
+  nameDiv.textContent = nameOfEpisode;
+  rootElem.appendChild(nameDiv);
+
+  const nameOfSeason = episode.season;
+  
+  const episodeNumber = episode.number;
+
+  const episodeCodeDiv = document.createElement("div");
+  if(episodeNumber < 10){
+  episodeCodeDiv.textContent = `S0${nameOfSeason}E0${episodeNumber}`;
+  rootElem.appendChild(episodeCodeDiv);
+  }
+  else if(episodeNumber >= 10){
+    episodeCodeDiv.textContent = `S0${nameOfSeason}E${episodeNumber}`;
+    rootElem.appendChild(episodeCodeDiv);
+  }
+   const episodeMediumImage = episode.image.medium;
+   const episodeMediumImageDiv = document.createElement("img");
+   episodeMediumImageDiv.src = episodeMediumImage;
+   rootElem.appendChild(episodeMediumImageDiv);
+
+   const episodeSummary = episode.summary;
+   const episodeSummaryDiv = document.createElement("div");
+   episodeSummaryDiv.innerHTML = episodeSummary;
+   rootElem.appendChild(episodeSummaryDiv);
+  }
 }
+
 
 window.onload = setup;
