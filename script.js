e609df3b7758e45d2f3156313be783248ca70fd4
Pamelalun <105378916+Pamelalun@users.noreply.github.com>
2023-02-02 00:17:50 +0000

    level 100 & 200

--- script.js
+++ script.js
@@ -2,16 +2,24 @@
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
+  searchItem();
 }
+
+
+
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.innerHTML = "";
+  const allEpisodesDisplay = document.getElementById("all-Episodes");
+  allEpisodesDisplay.textContent = `Got ${episodeList.length} episode(s)`;
 
   for (const episode of episodeList) {
+  const section =document.createElement("section");
+  rootElem.appendChild(section);
   const nameOfEpisode = episode.name;
   const nameDiv = document.createElement("div");
   nameDiv.textContent = nameOfEpisode;
-  rootElem.appendChild(nameDiv);
+  section.appendChild(nameDiv);
 
   const nameOfSeason = episode.season;
   
@@ -20,23 +28,45 @@ function makePageForEpisodes(episodeList) {
   const episodeCodeDiv = document.createElement("div");
   if(episodeNumber < 10){
   episodeCodeDiv.textContent = `S0${nameOfSeason}E0${episodeNumber}`;
-  rootElem.appendChild(episodeCodeDiv);
+  section.appendChild(episodeCodeDiv);
   }
   else if(episodeNumber >= 10){
     episodeCodeDiv.textContent = `S0${nameOfSeason}E${episodeNumber}`;
-    rootElem.appendChild(episodeCodeDiv);
+    section.appendChild(episodeCodeDiv);
   }
    const episodeMediumImage = episode.image.medium;
    const episodeMediumImageDiv = document.createElement("img");
    episodeMediumImageDiv.src = episodeMediumImage;
-   rootElem.appendChild(episodeMediumImageDiv);
+   section.appendChild(episodeMediumImageDiv);
 
    const episodeSummary = episode.summary;
    const episodeSummaryDiv = document.createElement("div");
    episodeSummaryDiv.innerHTML = episodeSummary;
-   rootElem.appendChild(episodeSummaryDiv);
+   section.appendChild(episodeSummaryDiv);
   }
 }
+
+
+function searchItem() {
+  const allEpisodes = getAllEpisodes();
+  const liveSearch = document.getElementById("research-bar");
+  liveSearch.addEventListener("keyup", (event) => {
+    const keyValues = event.target.value.toLowerCase();
+    const episodeFilter = allEpisodes.filter((searchedEpisodes) => {
+      return searchedEpisodes.name.toLowerCase().includes(keyValues) ||
+      searchedEpisodes.summary.toLowerCase().includes(keyValues);
+    });
+    makePageForEpisodes(episodeFilter);
+  });
+
+}
+
+const selectElement = document.getElementById('drop-menu');
+
+selectElement.addEventListener('change', (event) => {
+  const result = document.getElementById(allEpisodes);
+  result.textContent = `You like ${event.target.value}`;
+});
 
 
 window.onload = setup;
